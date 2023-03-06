0b11ac75a48b1337a5ae405b655e9ab92f82f174
dzulfaqaar <fikri6aul@gmail.com>
2023-03-06 15:35:24 +0700

    first commit

---
+++ example1.cpp
@@ -0,0 +1,24 @@
+//Library
+#include <iostream>
+using namespace std;
+
+//program main
+int main(){
+    double p,l,luas;
+    cout<<"masukan panjang =";
+    cin>>p;
+    cout<<"masukan lebar =";
+    cin>>l;
+    luas=p*l;
+    cout<<"luas persegi panjang adalah "<<luas;
+
+}
+
+   
+
+
+
+
+
+
+
